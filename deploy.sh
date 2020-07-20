8cd3c34ba76da31293d707ba0b82fa0e2257198a
sanyaojiuchan <sanyaojiuchan@126.com>
2020-07-20 22:26:30 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f https://github.com/sanyaojiuchan/sanyaojiuchan.github.io.git master
+git push -f git@github.com:sanyaojiuchan/sanyaojiuchan.github.io.git master
 
 cd -
